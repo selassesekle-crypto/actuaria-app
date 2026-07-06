3d87b02019a112b6ed5d72659e2678dd41098212
selassesekle-crypto <selassesekle@gmail.com>
2026-07-06 16:50:28 +0200

    SP Rapport: __init__.py AgentSPRapportActuariel

---
+++ direction_sante_prevoyance/rapport_actuariel/__init__.py
@@ -0,0 +1,7 @@
+"""
+Rapport Actuariel Santé-Prévoyance
+Direction Santé-Prévoyance — Équivalent SP de A7 Ibrahim
+"""
+from .agent import AgentSPRapportActuariel
+
+__all__ = ["AgentSPRapportActuariel"]
