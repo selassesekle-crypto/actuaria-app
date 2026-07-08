e5120ce9d5fc6a9a5e47ce7ee81533cc9eb0b4fd
selassesekle-crypto <selassesekle@gmail.com>
2026-07-08 10:57:41 +0200

    update: EP5 Reporting nouvelle version

--- direction_vie_epre/epargne_retraite/ep5_reporting/agent.py
+++ direction_vie_epre/epargne_retraite/ep5_reporting/agent.py
@@ -45,8 +45,8 @@ class AgentEP5ReportingEpargne:
 
     def __init__(
         self,
-        models_path: str = '/tmp/actuaria',
-        audit_path:  str = '/tmp/actuaria',
+        models_path: str = '/content/drive/MyDrive/ActuarIA/models',
+        audit_path:  str = '/content/drive/MyDrive/ActuarIA/audit',
         verbose:     bool = True
     ):
         self.models_path = Path(models_path)
